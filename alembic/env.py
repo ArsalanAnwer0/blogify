a7e6a3939b2650f909e65227c3590b31ab21021b
Muhammad Arsalan <arsalan.anwer9050@gmail.com>
2026-05-27 19:36:03 -0500

    fix: read database URL from env instead of hardcoding in alembic.ini

--- alembic/env.py
+++ alembic/env.py
@@ -2,8 +2,12 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from sqlalchemy import create_engine
 from alembic import context
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -59,11 +63,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_engine(os.getenv("DATABASE_URL"))
 
     with connectable.connect() as connection:
         context.configure(
